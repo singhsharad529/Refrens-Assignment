9f4563ffa673f183a5939c56e57f4589119b319b
Sharad Kumar Singh <singhsharad529@gmail.com>
2023-12-18 19:26:01 +0530

    added routing for episodes,locations and characters component with active feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { Header } from './components/header/Header'
-import { Characters } from './components/charactersCards/Characters'
-
-
+import { useState } from "react";
+import "./App.css";
+import { Header } from "./components/header/Header";
+import { Characters } from "./components/charactersCards/Characters";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import { Locations } from "./components/locationCards/Locations";
+import { Episodes } from "./components/episodesCards/Episodes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header/>
-      <Characters/>
+      <Router>
+        <Header />
+        <div className="middle-section container">
+          <Routes>
+            <Route path="/" element={<Navigate to="/characters" />} />
+            <Route exact path="/characters" element={<Characters />} />
+            <Route exact path="/episodes" element={<Episodes />} />
+            <Route exact path="/locations" element={<Locations />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,60 @@
-import './header.css'
+import { Link, NavLink } from "react-router-dom";
+import "./header.css";
 
 /*
 Header Component
 */
 
-export const Header=()=>{
-    return(
-        <>
-        <div>
+export const Header = () => {
+  return (
+    <>
+      <div>
         <nav className="navbar">
-        <div className="navbar-container container">
-            <input type="checkbox" name="" id=""/>
+          <div className="navbar-container container">
+            <input type="checkbox" name="" id="" />
             <div className="hamburger-lines">
-                <span className="line line1"></span>
-                <span className="line line2"></span>
-                <span className="line line3"></span>
+              <span className="line line1"></span>
+              <span className="line line2"></span>
+              <span className="line line3"></span>
             </div>
             <ul className="menu-items">
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Locations</a></li>
-                <li><a href="#">Episodes</a></li>
+              <li>
+                <NavLink
+                  to="/characters"
+                  className={({ isActive }) =>
+                    isActive ? "header-active" : ""
+                  }
+                >
+                  Characters
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/locations"
+                  className={({ isActive }) =>
+                    isActive ? "header-active" : ""
+                  }
+                >
+                  Locations
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/episodes"
+                  className={({ isActive }) =>
+                    isActive ? "header-active" : ""
+                  }
+                >
+                  Episodes
+                </NavLink>
+              </li>
             </ul>
-            <h1 className="logo">Refrens<sup className="logo-subheading">Assignment</sup></h1>
-        </div>
+            <h1 className="logo">
+              Refrens<sup className="logo-subheading">Assignment</sup>
+            </h1>
+          </div>
         </nav>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
